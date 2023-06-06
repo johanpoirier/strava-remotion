f4725e5ca7073c9664633e8b8c0cdd45f25676c1
Johan Poirier <johan.poirier@gmail.com>
2023-06-06 17:46:40 +0200

    Try spring animation on activity data

--- src/components/Activity/index.tsx
+++ src/components/Activity/index.tsx
@@ -1,25 +1,33 @@
 import React, { useMemo } from 'react';
+import { Img, spring, staticFile, useCurrentFrame, useVideoConfig } from 'remotion';
 import './style.css';
 import { DISPLAY_FRAME_RATIO, ACTIVITY_VIDEO_DURATION, FRAME_PER_SECOND } from '../../tools/constants';
+import { getActivityLogo } from '../../tools/activity-logo';
 import { MyActivity } from '../../models/MyActivity';
 import ActivityHeader from './ActivityHeader';
 import ActivityMap from './ActivityMap';
-import { Img, staticFile } from 'remotion';
-import { getActivityLogo } from '../../tools/activity-logo';
 
 export default function Activity({ data }: { data: MyActivity }) {
+  const frame = useCurrentFrame();
+  const { fps } = useVideoConfig();
+
   const pointsPerFrame = useMemo(() => {
     return Math.ceil(data.map.length / ((DISPLAY_FRAME_RATIO * ACTIVITY_VIDEO_DURATION * FRAME_PER_SECOND) / 1000));
   }, [data.map]);
 
+  const scale = spring({
+    fps,
+    frame,
+  });
+
   return (
     <section className="activity">
       <ActivityHeader data={data} />
-      {/*<div className="ActivityData">*/}
-      {/*  <span className="ActivityDataDetail">{Math.round(data.distance / 1000)} km</span>*/}
-      {/*  <span className="ActivityDataDetail">{Math.round(data.duration / 60)} min</span>*/}
-      {/*  <span className="ActivityDataDetail">{Math.round(data.elevationGain)}m+</span>*/}
-      {/*</div>*/}
+      <div className="activity-data" style={{ transform: `scale(${scale})` }}>
+        <span className="activity-data-detail">{Math.round(data.distance / 1000)} km</span>
+        <span className="activity-data-detail">{Math.round(data.duration / 60)} min</span>
+        <span className="activity-data-detail">{Math.round(data.elevationGain)}m+</span>
+      </div>
       <ActivityMap id={data.id} pointsPerFrame={pointsPerFrame} coordinates={data.map} />
       <Img className="activity-logo" src={staticFile(`assets/${getActivityLogo(data.type)}`)} alt={data.type} />
     </section>
